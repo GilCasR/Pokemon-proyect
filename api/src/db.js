41f90525b22cb108f8b9040bfb536065e0cad8db
Gilberto Castaño <gilberto.castano@outlook.es>
2023-03-23 19:27:27 -0500

    DB commit

--- api/src/db.js
+++ api/src/db.js
@@ -6,11 +6,9 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-   }
+   { logging: false, native: false }
 );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -39,10 +37,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, {through: "PokemonType"});
+Type.belongsToMany(Pokemon, {through: "PokemonType"});
 
 module.exports = {
    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    health: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    defence: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    timestamps: false
   });
 };
